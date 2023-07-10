9b70cf1d069aa7f63a6bfbbf89a57581c5cd1198
Sreedhar2484 <nagallisreedhar1918@gmail.com>
2023-07-10 21:03:45 +0530

    Exceptions

---
+++ ExceptionExample.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Custom exception derived from the base Exception class
+public class MyCustomException : Exception
+{
+    public MyCustomException()
+    {
+    }
+
+    public MyCustomException(string message)
+        : base(message)
+    {
+    }
+
+    public MyCustomException(string message, Exception innerException)
+        : base(message, innerException)
+    {
+    }
+}
+public class NotFoundException : Exception
+{
+    public NotFoundException()
+    {
+
+    }
+    public NotFoundException(string message) : base(message)
+    {
+
+    }
+    public NotFoundException(string message, Exception innerException) : base(message, innerException)
+    {
+
+    }
+
+
+    public class UserRepositry
+    {
+        public User GetUserById(int id)
+        {
+            User user = null;
+            if (user == null)
+            {
+                throw new NotFoundException($"User with ID {id} not found.");
+            }
+            return user;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿
+
+using static NotFoundException;
+
+// Example usage of the custom exception
+public class Program
+{
+    public static void Main()
+    {
+        try
+        {
+            // Throw the custom exception
+            throw new MyCustomException("This is a custom exception.");
+        }
+        catch (MyCustomException ex)
+        {
+            // Handle the custom exception
+            Console.WriteLine("Custom exception caught: " + ex.Message);
+        }
+        catch (Exception ex)
+        {
+            // Handle other exceptions
+            Console.WriteLine("An error occurred: " + ex.Message);
+        }
+
+        try
+        {
+            UserRepositry userRepositry = new UserRepositry();
+            User user = userRepositry.GetUserById(19189);
+        }
+        catch(NotFoundException ex)
+        {
+            Console.WriteLine("User not found: " + ex.Message);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("An error occurred: " + ex.Message);
+        }
+    }
+}
